c3b9384ff453bffda13721c7684c142cea5ffc85
antonio.gallo <antonio.gallo@fhoster.com>
2016-04-08 15:50:14 +0200

    reverted commonjs compliant in order to create it into another specific branch and make another pull request

--- index.js
+++
@@ -1,7 +0,0 @@
-require('./angular-restheart.module');
-require('./angular-restheart.config');
-
-require('./services/frh.factory');
-require('./services/rh.factory');
-require('./services/rhauth.service');
-require('././services/rhlogic.factory');
