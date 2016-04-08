8557e22057b4c80c17570f07416efd6e1cc640eb
Antonio Gallo <dokkis@hotmail.it>
2016-04-08 16:23:28 +0200

    Fix TypeError: Cannot read property 'go' of undefined (#5)
    * fix TypeError: Cannot read property 'go' of undefined due to restheart.onUnauthenticated() without $location and $state parameters
    
    * support for commonjs introducing a index.js file and changing the main file to index.js into the package.json
    
    * reverted commonjs compliant in order to create it into another specific branch and make another pull request

--- services/rhlogic.factory.js
+++ services/rhlogic.factory.js
@@ -38,7 +38,7 @@
                     });
                     // call configured call back, if any
                     if (angular.isFunction(restheart.onUnauthenticated)) {
-                        restheart.onUnauthenticated();
+                        restheart.onUnauthenticated($location, $state);
                     }
                 }
             }
