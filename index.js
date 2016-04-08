898cdefe325e6ca5d5c1dc97f5e084200d574a69
Antonio Gallo <dokkis@hotmail.it>
2016-04-08 16:25:55 +0200

    support for commonjs \ webpack (#6)
    * support for commonjs introducing a index.js file and changing the main file to index.js into the package.json
    
    * fix typos

---
+++ index.js
@@ -0,0 +1,7 @@
+require('./angular-restheart.module');
+require('./angular-restheart.config');
+
+require('./services/frh.factory');
+require('./services/rh.factory');
+require('./services/rhauth.service');
+require('./services/rhlogic.factory');
